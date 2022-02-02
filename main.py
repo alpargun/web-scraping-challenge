85dc37346e22807e772966ee1f133cafb96657e4
Alp Argun <alpargun@gmail.com>
2022-02-02 03:03:42 +0300

    find programs from json file. Add sleep before getting amounts

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 
-#%% Import statements
-import requests
-import pandas as pd
-from selenium import webdriver
-from bs4 import BeautifulSoup
-from lxml import html
-
-
 # %% STEP 1: Data Extraction 1
 # Save program name and its url
 
-# Download webdriver for a browser and add its path
-driver = webdriver.Chrome(executable_path='/Users/alp/Desktop/chromedriver')
-(driver.page_source).encode('utf-8')
-driver.get('https://bugcrowd.com/programs')
+import requests
+import pandas as pd
+import json
 
-# After inspecting the website, the elements with the class name: bc-panel__title contain name and urls of the programs
-panel_titles = driver.find_elements_by_class_name('bc-panel__title')
+
+source_url = "https://bugcrowd.com"
+json_url = source_url + "/programs.json"
+
+content = requests.get(json_url).text
+parsed_json = json.loads(content)
 
 p_names = [] # list of program names
 p_urls = [] # list of program urls
 
-for item in panel_titles:
-    p_names.append(item.text)
-    p_urls.append(item.find_element_by_css_selector('a').get_attribute('href'))
+for idx, val in enumerate(parsed_json['programs']):
+    p_names.append(val['name'])
+    p_urls.append(source_url + val['program_url'])
 
 # Add names and urls to a dataframe and convert to csv
 table = pd.DataFrame()
@@ -32,8 +27,17 @@ table['url'] = p_urls
 table.to_csv('data/program-list.csv', index=False)
 
 
+# -------------------------------------------------------------------------------
 #%% STEP 2: Data Extraction 2
 # Read csv file and retrieve program information for each program
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+# Download webdriver for a browser and add its path
+driver = webdriver.Chrome(executable_path='/Users/alp/Desktop/chromedriver')
+driver.page_source.encode('utf-8') # set charset to utf-8
 
 df = pd.read_csv('data/program-list.csv') # read again to comply with the task
 
@@ -42,19 +46,16 @@ info_dict = dict() # Keep the info as a dictionary to see corresponding program 
 for index, row in df.iterrows():
     driver.get(row['url']) # Go to url of the program
 
-    # In case the url does not have 'Program Details' tab, fill as empty string
-    try:
-        info = driver.find_element_by_class_name('bounty-content')
-        info_dict[row['name']] = info.text
-    except:
-        info = ''
-        info_dict[row['name']] = info
+    time.sleep(0.2) # wait to load all content
+    info = driver.find_element(By.CLASS_NAME, 'bounty-content')
+    info_dict[row['name']] = info.text
 
+
+#--------------------------------------------------------------------------------
 #%% STEP 3: Data Analysis
 # Read program information and extract min and max dollar amounts. 
 
 import re
-
 
 for key in info_dict:
     print(key)
@@ -62,7 +63,7 @@ for key in info_dict:
     print(dollars)
 
 
-
+# TODO keep max and min amounts in table
 
 
 
